2a12657b9914adfc283562d50b9ec0caff0cc764
Miguel-CaAr <castroarredondo.hector@gmail.com>
2024-07-08 12:46:01 -0700

    08/07/2024
    - [x] Remove permission

--- posts/api/views.py
+++ posts/api/views.py
@@ -9,6 +9,6 @@ class PostApiViewSet(ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.filter(published=True)
     lookup_field = 'slug'
-    permission_classes = [IsAdminReadOnly]
+    # permission_classes = [IsAdminReadOnly]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['id', 'title', 'category__title']
