78dc1ba0e2dfc8486994b2f547030507e24bbb46
Miguel-CaAr <castroarredondo.hector@gmail.com>
2024-07-09 00:03:06 -0700

    09/07/2024
    - [x] Orgering posts in reponse.

--- posts/api/views.py
+++ posts/api/views.py
@@ -3,12 +3,14 @@ from ..models import Post
 from .serializers import PostSerializer
 from .permissions import IsAdminReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.filters import OrderingFilter
 
 class PostApiViewSet(ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.filter(published=True)
     lookup_field = 'slug'
     # permission_classes = [IsAdminReadOnly]
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['id', 'title', 'category__title']
+    ordering_field = ['created_at']
+    ordering = ["-created_at"]
