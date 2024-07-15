5267081830bdfc97814ef938422df3c30d70b7e3
Miguel-CaAr <castroarredondo.hector@gmail.com>
2024-07-15 01:53:12 -0700

    fix(settings):

--- blog/settings.py
+++ blog/settings.py
@@ -31,12 +31,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY_DJANGO')
 DEBUG =  os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = [
-    f'{os.environ.get("CORS_ALLOWED_ORIGINS")}',
+    '*',
 ]
 
 # Aquí puedes agregar los dominios permitidos para CORS
 CORS_ALLOWED_ORIGINS = [
-    f'{os.environ.get("CORS_ALLOWED_ORIGINS")}',
+    '*',
 ]
 
 # Application definition
