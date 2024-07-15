f9514e43be4f4877e150a9053d8de1c6f7629ae7
Miguel-CaAr <castroarredondo.hector@gmail.com>
2024-07-15 01:40:04 -0700

    fix(settings): variables of enviroments for deploy

--- blog/settings.py
+++ blog/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_DJANGO')
 DEBUG =  os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = [
-    # os.environ.get('CORS_ALLOWED_ORIGINS'),
+    f'{os.environ.get("CORS_ALLOWED_ORIGINS")}',
 ]
 
 # Aquí puedes agregar los dominios permitidos para CORS
