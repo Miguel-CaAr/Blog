4abd06a18df678497026eaf67ccdc84d630e566a
Miguel-CaAr <144382056+Miguel-CaAr@users.noreply.github.com>
2024-07-19 14:15:21 -0700

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -31,15 +31,15 @@ SECRET_KEY = os.environ.get('SECRET_KEY_DJANGO')
 DEBUG =  os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = [
-    'localhost',
-    os.environ.get('ALLOWED_HOST_BACK'),
-    os.environ.get('ALLOWED_HOST_FRONT')
+    # 'localhost',
+    # os.environ.get('ALLOWED_HOST_BACK'),
+    # os.environ.get('ALLOWED_HOST_FRONT')
 ]
 
 # Aquí puedes agregar los dominios permitidos para CORS
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    os.environ.get('CORS_ALLOWED_ORIGIN_FRONT')
+    # 'http://localhost:5173',
+    # os.environ.get('CORS_ALLOWED_ORIGIN_FRONT')
 ]
 
 # Permitir credenciales
